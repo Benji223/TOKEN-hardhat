1e4d575aba3bec08e62aa476f671d33da3b513af
Benji223 <97120970+Benji223@users.noreply.github.com>
2024-02-15 15:05:36 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,10 @@ const hre = require('hardhat')
 
 
 /** Set contract and collection name **/
-const CONTRACT_NAME = 'Token'
-const TOKEN_NAME = 'Test1A'
-const TOKEN_SYMBOL = 'T1A'
-const ADMIN_ADDRESS = ''
+const CONTRACT_NAME = 'SimpleToken'
+const TOKEN_NAME = 'Test2A'
+const TOKEN_SYMBOL = 'T2A'
+const ADMIN_ADDRESS = '0xFa2a4b0358B132B261E796fa07D941842f3871D6'
 
 /** Main deploy function **/
 async function main() {
@@ -20,45 +20,16 @@ async function main() {
   )
 
   await contract.waitForDeployment()
-  // Print our newly deployed contract address
-  console.log("Contract deployed at ", await contract.getAddress())
+
+  const contractAdress = await contract.getAddress()
+
+  console.log("Contract deployed at ", contractAdress)
+
+await hre.run("verify:verify", { address: contractAdress,
+  constructorArguments: [TOKEN_NAME, TOKEN_SYMBOL, ADMIN_ADDRESS], })
 }
 /** Run Main function - Do not change **/
 main().catch((error) => {
   console.error(error)
   process.exitCode = 1
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
